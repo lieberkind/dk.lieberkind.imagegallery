7c81516e4a5047df14a97d8b968eb684b63b55d7
Tomas Lieberkind <t.lieberkind@gmail.com>
2015-05-13 20:58:07 +0200

    Cleaned up MainActivity

--- app/src/main/java/dk/lieberkind/imagegallery/MainActivity.java
+++ app/src/main/java/dk/lieberkind/imagegallery/MainActivity.java
@@ -8,54 +8,88 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 
+public class MainActivity extends FragmentActivity
+        implements ThumbnailFragment.OnThumbnailClickedListener {
 
-public class MainActivity extends FragmentActivity implements ThumbnailFragment.OnThumbnailClickedListener {
+    private final String THUMBNAIL_FRAGMENT_TAG = "thumbnail";
 
-    private ImageCache imageCache = ImageCache.getInstance();
+    /**
+     * The ImageCache from where already fetched images can be retrieved
+     */
+    private ImageCache mImageCache = ImageCache.getInstance();
 
-    private ArrayList<Image> images = new ArrayList<>();
+    /**
+     * The list of images that will be used by the GalleryFragment and ThumbnailFragment
+     */
+    private ArrayList<Image> mImages = new ArrayList<>();
 
+    /**
+     * The onCreate lifecycle method
+     *
+     * @param savedInstanceState Bundle containing the saved instance state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the activity's view
         setContentView(R.layout.activity_main);
 
+        // Add the ThumbnailFragment to the frame. We only want to do this the first time  onCreate
+        // is called, as the fragment manager will re-add the last active fragment to the frame on
+        // configuration changes. Failing to make this check results in several instances of the
+        // fragment being placed on top of each other within the frame. We add the fragment with a
+        // tag, so we can find it in the FragmentManager at a later time
         if(savedInstanceState == null) {
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction transaction = fm.beginTransaction();
-            transaction.add(R.id.frame, new ThumbnailFragment(), "thumbnails");
+            transaction.add(R.id.frame, new ThumbnailFragment(), THUMBNAIL_FRAGMENT_TAG);
             transaction.commit();
         }
 
-        if(!imageCache.empty()) {
-            images = imageCache.all();
+        // If the image cache has images, we use them in the activity. Only if the cache is empty
+        // we will download them from the server
+        if(!mImageCache.empty()) {
+            mImages = mImageCache.all();
         } else {
             new DownloadImagesTask().execute();
         }
     }
 
+    /**
+     * The handler for when a thumbnail is clicked in the ThumbnailFragment as required by the
+     * ThumbnailFragment.OnThumbnailClickedListener interface
+     *
+     * Here we replace the fragment which is currently in the frame with a new GalleryFragment
+     *
+     * @param position The position of the thumbnail clicked in the list of thumbnails
+     */
     @Override
     public void onThumbnailClicked(int position) {
-        System.out.println("I was pressed");
-
-        GalleryFragment gf = GalleryFragment.create(images, position);
+        GalleryFragment galleryFragment = GalleryFragment.create(mImages, position);
 
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.frame, gf);
+        transaction.replace(R.id.frame, galleryFragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
+    /**
+     * Asynchronous task for downloading images
+     */
     private class DownloadImagesTask extends AsyncTask<Void, Void, ArrayList<Image>> {
 
-        private ImageFetcher imageFetcher;
+        /**
+         * The ImageFetcher in use
+         */
+        private ImageFetcher mImageFetcher;
 
         /**
          * Create an instance of this task
          */
         public DownloadImagesTask() {
-            imageFetcher = new SequentialImageFetcher();
+            mImageFetcher = new SequentialImageFetcher();
         }
 
         /**
@@ -66,7 +100,7 @@ public class MainActivity extends FragmentActivity implements ThumbnailFragment.
          */
         @Override
         protected ArrayList<Image> doInBackground(Void... arguments) {
-            return imageFetcher.fetch();
+            return mImageFetcher.fetch();
         }
 
         /**
@@ -76,17 +110,20 @@ public class MainActivity extends FragmentActivity implements ThumbnailFragment.
          */
         @Override
         protected void onPostExecute(ArrayList<Image> images) {
-
-            System.out.println("AsyncTask: onPostExecute");
-
             if(images != null) {
-                MainActivity.this.images = images;
+                // Set the images in the activity
+                MainActivity.this.mImages = images;
 
+                // Add all images to the cache for later use
                 for(Image image : images) {
-                    imageCache.addImage(image);
+                    mImageCache.addImage(image);
                 }
 
-                ThumbnailFragment thumbnails = (ThumbnailFragment) getSupportFragmentManager().findFragmentByTag("thumbnails");
+                // Find the ThumbnailFragment and tell it to use the images as thumbnails
+                ThumbnailFragment thumbnails =
+                        (ThumbnailFragment) getSupportFragmentManager()
+                            .findFragmentByTag(THUMBNAIL_FRAGMENT_TAG);
+
                 thumbnails.setImages(images);
             }
         }
